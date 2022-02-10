fcf4bd567ae091c60759b1bff7e70ef19e46fe23
Harsh_1337 <harshanand.jha@gmail.com>
2022-02-10 11:29:15 +0530

    Added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import styled from "styled-components";
+const Wrapper = styled.div`
+  background-color: #e9dac1;
+  h1 {
+    margin-top: 0;
+    padding-top: 4vh;
+    padding-bottom: 2vh;
+    text-align: center;
+    font-family: "Bebas Neue", cursive;
+    font-size: 5rem;
+    color: ;
+  }
+`;
 export const Header = () => {
   return (
-    <div>
+    <Wrapper>
       <h1>To Do List</h1>
-    </div>
+    </Wrapper>
   );
 };
 export default Header;
